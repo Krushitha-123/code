3556c7aa70e73c894990a1a32edabe0fe2534f03
Krushitha-123 <150991313+Krushitha-123@users.noreply.github.com>
2023-11-28 10:59:39 +0530

    Update login.php

--- login.php
+++ login.php
@@ -9,3 +9,15 @@ echo "My first PHP script!";
 </body>
 </html> 
 hi hello how
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+ECHO "Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World!<br>";
+?>
+
+</body>
+</html>
